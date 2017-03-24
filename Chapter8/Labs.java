6a06ad62daa77d44c17fd6e0dbc8f7c0275d909b
AaronZeeb <aaronzeeb2@gmail.com>
2017-03-24 15:07:27 -0500

    3/24

---
+++ Chapter8/Labs.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Labs
+{
+    public static void main(String[] args)
+    {
+       Scanner keyboard = new Scanner(System.in);
+    }
+    
+    public static double numbers()
+    {
+        System.out.println("Interger counter.");
+        System.out.println();
+        
+    }
+    
+    public static String palindrome()
+    {
+       System.out.println("Palendrome tester.")
+       System.out.println();
+       System.out.println("Enter a sentence:")
+       String line=keyboard.nextLine();
+       if(line.length()==0)
+       {
+        
+        }
+       else
+       {
+        
+        }
+    }
+    
+    public static double GCD()
+    {
+        System.out.println("GCD finder.");
+        System.out.println();
+        
+    }
+    
+    public static int Fund()
+    {
+        System.out.println("Fund raising counter.");
+        System.out.println();
+        
+    }    
+}
