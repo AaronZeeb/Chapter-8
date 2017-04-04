fee4053bb5fd5bb3fbb92c85c40270fbe2680786
AaronZeeb <aaronzeeb2@gmail.com>
2017-04-04 15:08:32 -0500

    4/4

--- Chapter8/Labs.java
+++ Chapter8/Labs.java
@@ -1,36 +1,63 @@
 import java.util.Scanner;
 public class Labs
 {
-    public static void main(String[] args)
-    {
-       Scanner keyboard = new Scanner(System.in);
-    }
     
-    public static double numbers()
+    public static int Digit()
     {
-        System.out.println("Interger counter.");
+        System.out.println("Digit counter.");
         System.out.println();
-        
+        int num=50,rem=0;
+        if(num>10)
+        {
+            rem=num/10;
+            num++;
+        }
+        else
+            return num;
     }
     
     public static String palindrome()
     {
-       System.out.println("Palendrome tester.")
-       System.out.println();
-       System.out.println("Enter a sentence:")
-       String line=keyboard.nextLine();
-       if(line.length()==0)
-       {
-        
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println();
+        System.out.println("Palendrome tester.");
+        System.out.println();
+        System.out.println("Enter a sentence:");
+        String line=keyboard.nextLine();
+        int left=0, right=line.length()-1;
+        int pos=0;
+        left=,right=
+        left=
+        right=
+        if(left==right)
+        {
+            if(post<=(line.length()-1))
+            {
+            
+            }
         }
-       else
-       {
-        
-        }
+        while(line.charAt(left)==line.charAt(right)&& left<right)
+        {
+            left++;
+            right--;        
+         }
+        System.out.println();
+       
+        if(left<right)
+        {
+            String command="This is not a palindrome";
+            return line;
+         }
+        else
+        {
+            String an="This is a palindrome";
+            return an;
+            }
     }
-    
+    /*
     public static double GCD()
     {
+        System.out.println();
         System.out.println("GCD finder.");
         System.out.println();
         
@@ -38,8 +65,10 @@ public class Labs
     
     public static int Fund()
     {
+        System.out.println();
         System.out.println("Fund raising counter.");
         System.out.println();
         
-    }    
+    }  
+    */
 }
